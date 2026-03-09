eaf672c259ff2e79eab0f6cd6636bf0e6fa25e97
Tristen Miller <tristen.miller@mullen.pvt.k12.co.us>
2026-03-09 15:05:17 -0600

    Almost finished the change grade class. Can change the grade for any student for only the first period they are in

--- SIS/src/ChangeVariables.java
+++ SIS/src/ChangeVariables.java
@@ -10,15 +10,18 @@ public class ChangeVariables
 		static int studentClassChange;
 		static int firstClass;
 		static int secondClass;
+		static String changeGrade;
 		static String initialClass;
 		static String newClass;
 		static String initialClassGrade;
 		static String newClassGrade;
+		static int gradeToChange;
 		
 	// This class is used to change student grades and schedules
 	// changeClass() now functions properly
 		static Scanner userInput = new Scanner(System.in);
 		static Scanner userIntInput = new Scanner(System.in);
+		static Scanner userStringInput = new Scanner(System.in);
 			{
 				changeVariable();
 			}
@@ -34,19 +37,58 @@ public class ChangeVariables
 							 for (int i = 0; i < TextFile.directory.size(); i++)
 									{
 										System.out.println(i+1 + ") " +TextFile.directory.get(i).getFirstName() + " " + TextFile.directory.get(i).getLastName() + ":");
-										System.out.println("Period 1: " + TextFile.directory.get(i).getFirstPeriod() + ": " + TextFile.directory.get(studentClassChange).getFirstGrade());
-										System.out.println("Period 2: " + TextFile.directory.get(i).getSecondPeriod()+ ": " + TextFile.directory.get(studentClassChange).getSecondGrade());
-										System.out.println("Period 3: " + TextFile.directory.get(i).getThirdPeriod()+ ": " + TextFile.directory.get(studentClassChange).getThirdGrade());
+										System.out.println("Period 1: " + TextFile.directory.get(i).getFirstPeriod() + ": " + TextFile.directory.get(studentGradeChange).getFirstGrade());
+										System.out.println("Period 2: " + TextFile.directory.get(i).getSecondPeriod()+ ": " + TextFile.directory.get(studentGradeChange).getSecondGrade());
+										System.out.println("Period 3: " + TextFile.directory.get(i).getThirdPeriod()+ ": " + TextFile.directory.get(studentGradeChange).getThirdGrade());
 										System.out.println(" ");
 									}
 							 System.out.println("Enter the number of the student whose grade you wish to change.");
 							 studentGradeChange = (userIntInput.nextInt()) - 1;
-							 System.out.println("You have selected " + TextFile.directory.get(studentClassChange).getFirstName() + " " + TextFile.directory.get(studentClassChange).getLastName());
+							 System.out.println("You have selected " + TextFile.directory.get(studentGradeChange).getFirstName() + " " + TextFile.directory.get(studentGradeChange).getLastName());
 								System.out.println("Their Grades are:");
-								System.out.println("Period 1 Grade: " + TextFile.directory.get(studentGradeChange).getFirstPeriod() + ": " + TextFile.directory.get(studentClassChange).getFirstGrade());
-								System.out.println("Period 2 Grade: " + TextFile.directory.get(studentGradeChange).getSecondPeriod() + ": " + TextFile.directory.get(studentClassChange).getSecondGrade());
-								System.out.println("Period 3 Grade: " + TextFile.directory.get(studentGradeChange).getThirdPeriod() + ": " + TextFile.directory.get(studentClassChange).getThirdGrade());
+								System.out.println("Period 1 Grade: " + TextFile.directory.get(studentGradeChange).getFirstPeriod() + ": " + TextFile.directory.get(studentGradeChange).getFirstGrade());
+								System.out.println("Period 2 Grade: " + TextFile.directory.get(studentGradeChange).getSecondPeriod() + ": " + TextFile.directory.get(studentGradeChange).getSecondGrade());
+								System.out.println("Period 3 Grade: " + TextFile.directory.get(studentGradeChange).getThirdPeriod() + ": " + TextFile.directory.get(studentGradeChange).getThirdGrade());
 						
+							
+								System.out.println("Please enter the period number of the class you wish to change.");
+								firstClass = userIntInput.nextInt();
+								if (firstClass == 1)
+								{
+								initialClass = TextFile.directory.get(studentGradeChange).getFirstPeriod();
+								initialClassGrade = TextFile.directory.get(studentGradeChange).getFirstGrade();
+								}
+								else if (firstClass == 2)
+								{
+								initialClass = TextFile.directory.get(studentGradeChange).getSecondPeriod();
+								initialClassGrade = TextFile.directory.get(studentGradeChange).getSecondGrade();
+								}
+								else if (firstClass == 3) 
+								{
+								initialClass = TextFile.directory.get(studentGradeChange).getThirdPeriod();
+								initialClassGrade = TextFile.directory.get(studentGradeChange).getThirdGrade();
+								}
+								System.out.println("Please select a new letter grade for the period you have selected");
+								changeGrade = userStringInput.nextLine();
+								//TextFile.directory.get(studentGradeChange).setFirstGrade(changeGrade);
+								
+								if(gradeToChange == 1 )
+									{
+										TextFile.directory.get(studentGradeChange).setFirstGrade(changeGrade);
+									}
+								else if(gradeToChange == 2)
+									{
+										TextFile.directory.get(studentGradeChange).setSecondGrade(changeGrade);
+									}		
+								else if(gradeToChange == 3)
+									{
+										TextFile.directory.get(studentGradeChange).setThirdGrade(changeGrade);
+									}
+								System.out.println("The Grade for " + TextFile.directory.get(studentGradeChange).getFirstName() + " " + TextFile.directory.get(studentGradeChange).getLastName() + " is now:");
+								System.out.println("Period 1: " + TextFile.directory.get(studentGradeChange).getFirstPeriod() + ": " + TextFile.directory.get(studentGradeChange).getFirstGrade());
+								System.out.println("Period 2: " + TextFile.directory.get(studentGradeChange).getSecondPeriod() + ": " + TextFile.directory.get(studentGradeChange).getSecondGrade());
+								System.out.println("Period 3: " + TextFile.directory.get(studentGradeChange).getThirdPeriod() + ": " + TextFile.directory.get(studentGradeChange).getThirdGrade());
+								
 								// person put in student, then puts in class 
 								//
 								// when asked for new grade changeGrade = (userStringInput.nextLine)
